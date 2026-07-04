5821ce9b3f6d9ad079f2d903d11bc9b8b062746c
wahlequinn7-art <wahlequinn7@gmail.com>
2026-07-04 20:42:34 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+
+const app = express();
+
+app.use(cors());
+
+// Health check (Railway wichtig)
+app.get("/", (req, res) => {
+    res.send("Zug API läuft 🚆");
+});
+
+// 🔍 Locations (Autocomplete)
+app.get("/locations", async (req, res) => {
+
+    const q = req.query.q;
+
+    try {
+        const response = await fetch(
+            `https://v6.db.transport.rest/locations?query=${encodeURIComponent(q)}&results=6&poi=false&addresses=false`
+        );
+
+        const data = await response.json();
+        res.json(data);
+
+    } catch (err) {
+        res.status(500).json({ error: "Fehler locations" });
+    }
+});
+
+// 🚆 Journeys
+app.get("/journeys", async (req, res) => {
+
+    const { from, to } = req.query;
+
+    try {
+        const response = await fetch(
+            `https://v6.db.transport.rest/journeys?from=${from}&to=${to}&results=5`
+        );
+
+        const data = await response.json();
+        res.json(data);
+
+    } catch (err) {
+        res.status(500).json({ error: "Fehler journeys" });
+    }
+});
+
+// Railway nutzt PORT automatisch
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server läuft auf Port", PORT);
+});
